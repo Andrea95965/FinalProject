793131905dac2fd7a3695c863519fdcde7c15c48
Aedra Li <aedraizzy114@gmail.com>
2017-07-05 09:14:37 -0700

    preload first commit

---
+++ preload.js
@@ -0,0 +1,9 @@
+var preload ={
+    
+    preload: function(){
+        
+        this.game.load.image('redDino','redDino.png');
+        this.game.load.image('fatDino','fatDino.png');
+        this.game.load.imagez('spikyDino','spikyDino.png');
+    }
+}
