7613e09e07352dd5e9cae0c228e17eb5d760e3b6
Aedra Li <aedraizzy114@gmail.com>
2017-07-05 09:36:59 -0700

    gameover

--- preload.js
+++ preload.js
@@ -5,5 +5,5 @@ var preload ={
         this.game.load.image('redDino','redDino.png');
         this.game.load.image('fatDino','fatDino.png');
         this.game.load.imagez('spikyDino','spikyDino.png');
-    }
+        }
 }
