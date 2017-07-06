e46fe876783f5005c72c1d9dbacf93c5fdfa78e5
pamela415 <prcruz10303@yahoo.com>
2017-07-06 12:51:27 -0700

    added cake

--- preload.js
+++ preload.js
@@ -3,13 +3,7 @@ var preload ={
     preload: function(){
         
         this.game.load.image('fatDino','fatDino.png');
-        this.game.load.image('donut','donut.png');
-        this.game.load.image('steak','steak.png');
-        this.game.load.image('pizza','pizza.png');
-        this.game.load.image('popsicle','popsicle.png');
-        this.game.load.image('taco','taco.png');
-        this.game.load.image('sushi','sushi.png');
-        this.game.load.image('pancake','pancake.png');
+        this.game.load.image('food','cake.png')
         this.game.load.image('gameover','gameoverRed.png');
         this.game.load.image('loading','loading.png');
         this.game.load.image('start','startButton.png');
