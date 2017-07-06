b7a41768206f0be4ad25095dd7511d2739efce4c
Aedra Li <aedraizzy114@gmail.com>
2017-07-06 10:34:55 -0700

    added game title and grass

--- preload.js
+++ preload.js
@@ -2,9 +2,9 @@ var preload ={
     
     preload: function(){
         
-        this.game.load.image('redDino','redDino.png');
-        this.game.load.image('fatDino','fatDino.png');
-        this.game.load.image('spikyDino','spikyDino.png');
+        this.game.load.image('player1','redDino.png');
+        this.game.load.image('player2','fatDino.png');
+        this.game.load.image('player3','spikyDino.png');
         this.game.load.image('donut','donut.png');
         this.game.load.image('steak','steak.png');
         this.game.load.image('pizza','pizza.png');
@@ -15,5 +15,7 @@ var preload ={
         this.game.load.image('gameover','gameoverRed.png');
         this.game.load.image('loading','loading.png');
         this.game.load.image('start','startButton.png');
+        this.game.load.image('grass','grass.png');
+        this.game.load.image('gameTitle','gameTitle.png');
     }
 }
