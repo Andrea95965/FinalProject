0e22eb2013f195266a78b20236ee2db81da0e83f
Aedra Li <aedraizzy114@gmail.com>
2017-07-10 12:20:47 -0700

    adjusted the gametitle and start button

--- gametitle.js
+++ gametitle.js
@@ -1,8 +1,8 @@
 var gameTitle = {
     create: function(){
-        var gameTitle = this.game.add.sprite(250,100,'gameTitle');
+        var gameTitle = this.game.add.sprite(710,300,'gameTitle');
         gameTitle.anchor.setTo(0.5,0.5);
-        var startButton = this.game.add.button(250,100,'start',this.playTheGame,this);
+        var startButton = this.game.add.button(710,250,'start',this.playTheGame,this);
         startButton.anchor.setTo(0.5,0.5);
     },
     
