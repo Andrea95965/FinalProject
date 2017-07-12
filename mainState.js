96d92aa481b47e5211226889edca15029b8aab06
Andrea95965 <andrearamos310@gmail.com>
2017-07-12 09:15:30 -0700

    commit

--- mainState.js
+++ mainState.js
@@ -5,7 +5,7 @@ var mainState = {
         
         this.cursor = this.game.input.keyboard.createCursorKeys();
         
-        this.game.stage.backgroundColor = '#87CEEB';   
+        this.game.stage.backgroundColor ='#87CEEB';   
         
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
     
@@ -32,8 +32,8 @@ var mainState = {
         '!    o  o  o  !  o     o                      o o   x',
         '!xxxxxxxxxxxxxxxxxxx!!!!!!xxxxxxxxxxxxxxxxxxxxxxxxxxx',
     ];
-        for (var i = 0; i < level.length; i++) 
-            {for (var j = 0; j < level[i].length; j++) {
+        for (var i = 0; i < level.length; i++) {
+            for (var j = 0; j < level[i].length; j++) {
 
 
                 if (level[i][j] == 'x') {
