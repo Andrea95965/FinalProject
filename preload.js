1c3227129a003eeb876e60b248a6cd90c93d6642
Aedra Li <aedraizzy114@gmail.com>
2017-07-12 09:30:37 -0700

    fixed the game

--- preload.js
+++ preload.js
@@ -2,21 +2,23 @@ var preload ={
     
     preload: function(){
         var loadingBar = this.add.sprite(160,240,"loading");
+        console.log("loaded loading bar");
         loadingBar.anchor.setTo(0.5,0.5);
         this.load.setPreloadSprite(loadingBar);
         
         this.game.load.image('fatDino','newDino.png');
         this.game.load.image('food','cake.png');
-        this.game.load.image('gameover','gameoverRed.png');
-        this.game.load.image('loading','loading.png');
+//        this.game.load.image('gameover','gameoverRed.png');
         this.game.load.image('start','startbutton2.png');
         this.game.load.image('wall','grass.png');
         this.game.load.image('gameTitle','gameTitle.png');
         this.game.load.image('meteor','meteor.png');
         this.game.load.image('lava','lava.png');
+        console.log("loaded all assets");
     },
     
     create: function(){
-    this.game.state.start("gameTitle");
+        console.log("starting gameTitle");
+        this.game.state.start("gameTitle");
     }
 };
