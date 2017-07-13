7ed57fa52b7cf5a1163272c5663f7f007a286e4a
Aedra Li <aedraizzy114@gmail.com>
2017-07-13 12:50:46 -0700

    added end screen

---
+++ DINOMITE.js
@@ -0,0 +1,17 @@
+var dinoMite ={
+    create:function(){
+      var dinoMiteTitle =this.game.add.sprite(710,350,"dinoMite");
+      dinoMiteTitle.anchor.setTo(0.5,0.5);
+      var startButton = this.game.add.button(710,292,"start",this.playTheGame,this);
+      startButton.anchor.setTo(0.5,0.5);
+        
+    },
+
+    playTheGame:function(){
+        
+        this.game.state.start("state1");
+    
+    
+    }
+};
+
