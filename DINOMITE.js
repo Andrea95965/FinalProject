ed32b845a8df70d38b4c35cf25553d1c60befe56
Aedra Li <aedraizzy114@gmail.com>
2017-07-14 10:53:39 -0700

    end screen

--- DINOMITE.js
+++ DINOMITE.js
@@ -2,14 +2,14 @@ var dinoMite ={
     create:function(){
       var dinoMiteTitle =this.game.add.sprite(710,350,"dinoMite");
       dinoMiteTitle.anchor.setTo(0.5,0.5);
-      var startButton = this.game.add.button(710,292,"replay",this.playTheGame,this);
-      startButton.anchor.setTo(0.5,0.5);
+      var replayButton = this.game.add.button(710,292,"replay",this.playTheGame,this);
+      replayButton.anchor.setTo(0.5,0.5);
         
     },
 
     playTheGame:function(){
         
-        this.game.state.start("state1");
+        this.game.state.replay("state1");
     
     
     }
