75ab23ba950d146c531ada9697467dcadfd17630
Aedra Li <aedraizzy114@gmail.com>
2017-07-14 10:39:48 -0700

    dinomite

--- preload.js
+++ preload.js
@@ -15,7 +15,7 @@ var preload ={
         this.game.load.image('meteor','meteor.png');
         this.game.load.image('lava','lava.png'); 
         this.game.load.image('fireball','coolFireball.png')
-      
+       this.game.load.image('replay','New Piskel(2).png')
         console.log("loaded all assets");
     },
     
